c9db17ff84a724346c12e91a567998ae779c84e5
BlyznyukTaras <onthetime731@gmail.com>
2023-06-25 00:14:38 +0300

    Added another simple program

---
+++ src/ExamplePrograms/SecondSimpleProgram.java
@@ -0,0 +1,19 @@
+package ExamplePrograms;
+
+
+public class SecondSimpleProgram {
+    public static void main(String[] args) {
+        int number = 50;
+        System.out.println("The value of the first variable is " + number);
+        int anotherNumber = number * number;
+        System.out.println("The value of the second (number * number) variable is " + anotherNumber);
+        if (anotherNumber > number) System.out.println("Second variable's value is bigger than first's one");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("The iterable value here (number * i) is " + number * i);
+        }
+        while (number != 0) {
+            System.out.println(number);
+            number -= 10;
+        }
+    }
+}

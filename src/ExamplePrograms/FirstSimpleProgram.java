48265c76e34477a6fcd3440c39296b48c8fdadd5
BlyznyukTaras <onthetime731@gmail.com>
2023-06-24 21:16:42 +0300

    Refactoring

--- src/ExamplePrograms/FirstSimpleProgram.java
+++ src/ExamplePrograms/FirstSimpleProgram.java
@@ -1,3 +1,5 @@
+package ExamplePrograms;
+
 public class FirstSimpleProgram {
     public static void main(String[] args) {
         System.out.println("Here is some kind of string that is being shown!");
